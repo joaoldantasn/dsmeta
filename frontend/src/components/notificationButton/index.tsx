b1301a0afd9fef8c98702076e4076b2af2b9ee64
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2022-09-10 14:10:12 -0300

    Send notification

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,11 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
-function NotificatinButton() {
+
+type Props = {
+  saleId: number;
+} 
+
+function handleCLick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(reponse =>{
+    console.log("SUCESSO");
+  })
+}
+
+function NotificatinButton({saleId} : Props) {
   return(
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleCLick(saleId)}>
       <img src={icon} alt="Notificar" />
     </div>
   )
 }
 
-export default NotificatinButton
+export default NotificatinButton;
